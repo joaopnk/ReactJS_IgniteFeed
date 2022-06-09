09dc1701d557bae3495e7446a6ace9f14b6d1309
joaopnk <joaopedronk22@gmail.com>
2022-06-08 21:13:59 -0300

    08/06/2022
    Componente Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,28 @@
+import styles from './Post.module.css';
+
+export function  Post(){
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img 
+                        className={styles.avatar}
+                        src="https://avatars.githubusercontent.com/u/71463038?v=4" 
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>João Pedro</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="08 de junho as 21:08" dateTime="2022-06-08 21:08">Publicado há 1hr</time>
+            </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é IgniteFeed 🚀</p>
+                <p>👉 <a href="">github.com/joaopnk</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat #ignite</a></p>
+            </div>
+        </article>
+    )
+}
