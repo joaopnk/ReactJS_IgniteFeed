9f0d1dd87ef4de2bae8badfa42bed57a2eb55bbd
joaopnk <joaopedronk22@gmail.com>
2022-06-09 23:12:12 -0300

    .
    Componente: comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/71463038?v=4"  />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Pedro</strong>
+                            <time title="08 de junho as 21:08" dateTime="2022-06-08 21:08">Cerca de 1hr atrás</time>
+                        </div>
+
+
+                        <button title="Deletar comentário">
+                            <Trash 
+                                size={20}
+                            />
+                        </button>
+                    </header>
+
+                    <p>Muito bom JP, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
