e5784a65c86e7ce3878b71eabd2ef4e844142074
joaopnk <joaopedronk22@gmail.com>
2022-07-04 20:38:38 -0300

    04/07/22

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,41 @@ import './global.css';
 
 import styles from './App.module.css';
 
+// author: {avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
 
+// # Simulando dados vindo de uma API
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/joaopnk.png',
+      name: 'João Pedro',
+      role: 'Web developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é IgniteFeed 🚀'},
+      {type: 'link', content: 'github.com/joaopnk'},
+    ],
+    publishedAt: new Date('2022-07-04 20:10:10')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/joao.png',
+      name: 'João',
+      role: 'Web developer back-end'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é IgniteFeed 🚀'},
+      {type: 'link', content: 'github.com/joaopnk'},
+    ],
+    publishedAt: new Date('2022-07-01 20:05:10')
+  },
+];
 
 export function App() {
 
@@ -17,14 +51,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-              author="João Pedro"
-              content="lorem ipsum dolor sit amet, consectetur adipiscing"
-            />
-          <Post 
-              author="Pedro João"
-              content="lorem ipsum dolor sit amet, consectetur adipiscing"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
